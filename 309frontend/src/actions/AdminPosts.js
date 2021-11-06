02a2bac604b4b2eba1c98bbec7f64f29362c72dc
Kevin <72360522+kevincecco245@users.noreply.github.com>
2021-11-06 14:35:02 -0400

    Changed AdminPost Actions

--- 309frontend/src/actions/AdminPosts.js
+++ 309frontend/src/actions/AdminPosts.js
@@ -1,1 +1,32 @@
+export const denyApp = (panel, business) => {
+    const filteredApps = panel.state.business_applications.filter(b => {
+        return b !== business
+    });
 
+    panel.setState({
+        business_applications: filteredApps
+    })
+}
+
+export const acceptApp = (panel, business) => {
+    const filteredApps = panel.state.business_applications.filter(b => {
+        return b !== business
+    });
+
+    const busList = panel.state.businesses;
+    busList.push(business);
+    panel.setState({
+        business_applications: filteredApps,
+        businesses: busList
+    })
+}
+
+export const removeBusiness = (panel, business) => {
+    const filteredBus = panel.state.businesses.filter(b => {
+        return b !== business
+    });
+
+    panel.setState({
+        businesses: filteredBus
+    })
+}
