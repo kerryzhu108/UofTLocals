e672ed2878d4fdd31ab9df463875ba7e6d7e958c
Zachary Vincze <zacharyvincze@gmai.com>
2021-11-07 18:13:39 -0500

    Fixed more warnings

--- 309frontend/src/components/BusinessApp.js
+++ 309frontend/src/components/BusinessApp.js
@@ -6,21 +6,21 @@ import x from "../images/x.png";
 
 class BusinessApp extends React.Component {
     render() {
-        const { name, message, removeApp, addApp, email, date} = this.props
+        const { name, message, removeApp, addApp, email, date } = this.props
         return (
             <div className="business_app">
                 <div className="app_header">
                     <span className="app_header_span">{name}</span>
                     <button type="button" className="business_app_buttons" onClick={removeApp}>
-                        <img className='images' src={x} alt=""/>
+                        <img className='images' src={x} alt="" />
                     </button>
                     <button type="button" className="business_app_buttons" onClick={addApp}>
-                        <img className='images' src={checkmark} alt=""/>
+                        <img className='images' src={checkmark} alt="" />
                     </button>
                 </div>
                 <p className="business_app_text">
-                    {message} <br/>
-                    Email: {email} <br/>
+                    {message} <br />
+                    Email: {email} <br />
                     Date Applied: {date}
                 </p>
 
--- 309frontend/src/components/InputButtonCombo.js
+++ 309frontend/src/components/InputButtonCombo.js
@@ -5,7 +5,7 @@ import "../css/InputButtonCombo.css"
 class InputButtonCombo extends React.Component {
     render() {
         return (
-            <div class="input-button-combo">
+            <div className="input-button-combo">
                 <textarea type="text" onChange={this.props.onChange} />
                 <button className={this.props.color} onClick={this.props.onClick}>{this.props.buttonName}</button>
             </div>
