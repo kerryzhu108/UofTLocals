ddae1dda9be9911cea35362e33753353b7887859
Zachary Vincze <zacharyvincze@gmai.com>
2021-11-07 17:44:35 -0500

    Fixed some warnings

--- 309frontend/src/components/InputButtonCombo.js
+++ 309frontend/src/components/InputButtonCombo.js
@@ -7,7 +7,7 @@ class InputButtonCombo extends React.Component {
         return (
             <div class="input-button-combo">
                 <textarea type="text" onChange={this.props.onChange} />
-                <button class={this.props.color} onClick={this.props.onClick}>{this.props.buttonName}</button>
+                <button className={this.props.color} onClick={this.props.onClick}>{this.props.buttonName}</button>
             </div>
         );
     }
