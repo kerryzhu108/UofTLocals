66f5a44f10260aa8ad08e405cef0f0aa62e2fbd7
kerryzhu08 <jiayu.zhu@mail.utoronto.ca>
2021-12-01 22:17:47 -0500

    stop tracking .env file and change backend port

--- 309backend/src/server.js
+++ 309backend/src/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const ip = process.env.IP || "localhost";
 
 // Setup application middleware
