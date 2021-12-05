bb68eb44e2182c89daeff20f539b5a15ab1befb6
William Wang <williamm.wang@mail.utoronto.ca>
2021-12-04 21:07:40 -0500

    changing login api file to use async await

--- 309frontend/src/apis/login.js
+++ 309frontend/src/apis/login.js
@@ -10,198 +10,171 @@ export const updateLoginForm = (comp, field) => {
 }
 
 /* A function to send a POST request to register a new student account. */
-export const registerStudent = (comp, event) => {
+export const registerStudent = async (comp, event) => {
     event.preventDefault()
     // make sure the given password and confirmation match
     if (comp.state.password !== comp.state.confirmation) {
         alert('Passwords do not match.')
         return
     }
-    // make a call to the server API to register a student
-    fetch(`${domain}auth/register/student`, {
-        method: 'POST',
-        headers: headers,
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify({
-            "email": comp.state.username,
-            "password": comp.state.password,
-            "first_name": comp.state.firstname,
-            "last_name": comp.state.lastname
+    try {
+        // make a call to the server API to register a student
+        let response = await fetch(`${domain}auth/register/student`, {
+            method: 'POST',
+            headers: headers,
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify({
+                "email": comp.state.username,
+                "password": comp.state.password,
+                "first_name": comp.state.firstname,
+                "last_name": comp.state.lastname
+            })
         })
-    }).then((response) => {
-        if (response.status === 200) {
-            // status code 200, return the response body
-            return response.json()              
-        } else {
+        if (response.status !== 200) {
             // received a code other than 200
             alert(response.status + ' ' + response.statusText)
-        }
-    }).then((json) => {
-        if (json === undefined) {
-            // status code was not 200
             return
         }
-        if (json.email !== undefined) {
+        response = await response.json()
+        if (response.email !== undefined) {
             // successful login from the server. we have the id and access tokens now
-            alert('Signed up as: ' + json.email)
+            alert('Signed up as: ' + response.email)
             window.location.href = '/login'
-        } else if (json[0].msg !== undefined && json[0].param !== undefined) {
+        } else if (response[0].msg !== undefined && response[0].param !== undefined) {
             // alert the user of what went wrong, if we can
-            const msg = json[0].msg + ' for ' + json[0].param
+            const msg = response[0].msg + ' for ' + response[0].param
             alert(msg)
         }
-    }).catch((error) => { 
+    } catch (error) {
         // log any errors to the console
         console.log(error)
-    })
+    }
 }
 
 /* A function to send a POST request to register a new business account. */
-export const registerBusiness = (comp, event) => {
+export const registerBusiness = async (comp, event) => {
     event.preventDefault()
     // make sure the given password and confirmation match
     if (comp.state.password !== comp.state.confirmation) {
         alert('Passwords do not match.')
         return
     }
-    // make a call to the server API to register a business account
-    fetch(`${domain}auth/register/business`, {
-        method: 'POST',
-        headers: { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify({
-            "name": comp.state.name,
-            "email": comp.state.username,
-            "password": comp.state.password,
-            "desc": comp.state.description, 
+    try {
+        // make a call to the server API to register a business account
+        let response = await fetch(`${domain}auth/register/business`, {
+            method: 'POST',
+            headers: headers,
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify({
+                "name": comp.state.name,
+                "email": comp.state.username,
+                "password": comp.state.password,
+                "desc": comp.state.description, 
+            })
         })
-    }).then((response) => {
-        if (response.status === 200) {
-            // status code 200, return the response body
-            return response.json()              
-        } else {
+        if (response.status !== 200) {
             // received a code other than 200
             alert(response.status + ' ' + response.statusText)
-        }
-    }).then((json) => {
-        if (json === undefined) {
-            // status code was not 200
             return
         }
-        if (json.email !== undefined) {
+        response = await response.json()
+        if (response.email !== undefined) {
             // successful login from the server. we have the id and access tokens now
-            alert('Signed up as: ' + json.email)
+            alert('Signed up as: ' + response.email)
             window.location.href = '/login'
-        } else if (json[0].msg !== undefined && json[0].param !== undefined) {
+        } else if (response[0].msg !== undefined && response[0].param !== undefined) {
             // alert the user of what went wrong, if we can
-            const msg = json[0].msg + ' for ' + json[0].param
+            const msg = response[0].msg + ' for ' + response[0].param
             alert(msg)
         }
-    }).catch((error) => { 
+    } catch (error) {
         // log any errors to the console
         console.log(error)
-    })
+    }
 }
 
 /* A function to send a POST request to log a user in. */
-export const login = (comp, event) => {
+export const login = async (comp, event) => {
     event.preventDefault()
     // figure out whether we are to log in as a student or business
-    let url
-    if (comp.state.type === 'student') {
-        url = `${domain}auth/login/student`
-    } else {
-        url = `${domain}auth/login/business`
-    }
-    console.log(url)
-    // make a call to the server API to login
-    fetch(url, {
-        method: 'POST',
-        headers: { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        cache: 'default',
-        body: JSON.stringify({
-            "email": comp.state.username,
-            "password": comp.state.password,
+    const url = comp.state.type === 'student' ? `${domain}auth/login/student` : `${domain}auth/login/business`
+    
+    try {
+        // make a call to the server API to login
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify({
+                "email": comp.state.username,
+                "password": comp.state.password,
+            })
         })
-    }).then((response) => { 
-        if (response.status === 200) {
-            // status code 200, return the response body
-            return response.json()              
-        } else {
+        if (response.status !== 200) {
             // received a code other than 200
             alert(response.status + ' ' + response.statusText)
-        }
-    }).then((json) => {
-        if (json === undefined) {
-            // status code was not 200
             return
         }
-        if (json.tokens !== undefined) {
+        response = await response.json()
+        if (response.tokens !== undefined) {
             // successful login from the server. we have the id and access tokens now
             // Store received access token in cookie
             const cookies = new Cookies();
-            cookies.set("access_token", json.tokens.access, {path: "/"});
-
-            alert('You are being logged in as: ' + json.tokens.access)
+            cookies.set("access_token", response.tokens.access, {path: "/"});
+            alert('You are being logged in as: ' + response.tokens.access)
             window.location.href = '/'
-        } else if (json[0].msg !== undefined && json[0].param !== undefined) {
+        } else if (response[0].msg !== undefined && response[0].param !== undefined) {
             // alert the user of what went wrong, if we can
-            const msg = json[0].msg + ' for ' + json[0].param
+            const msg = response[0].msg + ' for ' + response[0].param
             alert(msg)
         }
-    }).catch((error) => { 
+    } catch (error) {
         // log any errors to the console
         console.log(error)
-    })
+    }
 }
 
-/* A function to send a GET request to check which user is logged in. */
-export const checkSession = () => {
-    const url = `${domain}auth/check-session`;
-    fetch(url)
-        .then(res => {
-            if (res.status === 200) {
-                return res.json();
-            }
-        })
-        .then(json => {
-            console.log(json)
-            alert('stop')
-            if (json && json.user) {
-                return json.user
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
+// /* A function to send a GET request to check which user is logged in. */
+// export const checkSession = () => {
+//     const url = `${domain}auth/check-session`;
+//     fetch(url)
+//         .then(res => {
+//             if (res.status === 200) {
+//                 return res.json();
+//             }
+//         })
+//         .then(json => {
+//             console.log(json)
+//             alert('stop')
+//             if (json && json.user) {
+//                 return json.user
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+// }
 
-/* A function to send a GET request to log the current user out. */
-export const logout = () => {
-    const url = `${domain}auth/logout`;
-    fetch(url)
-        .then(res => {
-            if (res.status === 200) {
-                return res.json();
-            }
-        })
-        .ethn(json => {
-            console.log(json)
-            alert('stop')
-            if (json && json.user) {
-                return json.user
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
+// /* A function to send a GET request to log the current user out. */
+// export const logout = () => {
+//     const url = `${domain}auth/logout`;
+//     fetch(url)
+//         .then(res => {
+//             if (res.status === 200) {
+//                 return res.json();
+//             }
+//         })
+//         .ethn(json => {
+//             console.log(json)
+//             alert('stop')
+//             if (json && json.user) {
+//                 return json.user
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
+// }
